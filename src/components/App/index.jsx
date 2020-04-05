bb05c87813e6dc5ac665843b85ffc1a1b31c96af
Durgesh Nandan Sinha <dnsinha33@gmail.com>
2020-04-05 23:38:07 +0530

    header implementation

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,13 +1,16 @@
 import React, { Component, Suspense } from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
 import Routes from 'Components/App/routes';
 import Loader from 'Components/loader';
+import Header from 'Components/headers';
 
 
 export class App extends React.Component {
     render() {
         return (
             <div>
+                <Header />
                 <Router>
                     <Suspense fallback={<Loader />}>
                         <Routes />
--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 
 export class Home extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      sample: 'hi'
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      sample: 'newHi'
+    })
+  }
+
   render() {
     return (
-        <div>
-          <p>Home Works!</p>
-          {process.env.GOOGLE_URL}
-        </div>
+      <div>
+        <p>Home Works!</p>
+        <p>{process.env.GOOGLE_URL}</p>
+        {this.state.sample}
+      </div>
     );
   }
 }
